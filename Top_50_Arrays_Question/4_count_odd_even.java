40ea17a4df63e3e210b4ac96cc323f7fee39d4f9
Shruti Srivastava <shrutishreya222@gmail.com>
2023-08-07 23:28:46 +0530

    Problem 4

--- Top_50_Arrays_Question/4_count_odd_even.java
+++ Top_50_Arrays_Question/4_count_odd_even.java
@@ -7,12 +7,8 @@ class Solution
         int even=0;
         int odd=0;
         for(int i=0;i<arr.length;i++){
-            if(arr[i]%2==0){
-                even++;
-            }
-            else if(arr[i]%2!=0){
-                odd++;
-            }
+            if(arr[i]%2==0)   even++;
+            else if(arr[i]%2!=0)    odd++;
         }
        System.out.println(odd + " " + even);
     }
